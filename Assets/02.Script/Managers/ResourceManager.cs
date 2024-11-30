dff751c657a86aaaf2546fbdad4daaa5422bf518
addkey0223 <addkey0223@gmail.com>
2024-11-30 11:54:07 +0900

    [기능] ResourceManager 추가

---
+++ Assets/02.Script/Managers/ResourceManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class ResourceManager : SingletonMonobehaviour<ResourceManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public T Load<T>(string path) where T : Object
+    {
+        if (typeof(T) == typeof(GameObject))
+        {
+            string name = path;
+            int index = name.LastIndexOf('/');
+            if (index >= 0)
+                name = name.Substring(index + 1);
+
+            // 만들어진 오브젝트가 있으면 풀에서 땡겨온다.
+            GameObject go = PoolManager.Instance.GetOriginal(name);
+            if (go != null)
+                return go as T;
+        }
+
+        return Resources.Load<T>(path);
+    }
+
+    public GameObject Instantiate(string path, Transform parent = null)
+    {
+        GameObject original = Load<GameObject>($"Prefabs/{path}");
+        if (original == null)
+        {
+            Debug.Log($"Failed to load prefab : {path}");
+            return null;
+        }
+
+        if (original.GetComponent<Poolable>() != null)
+            return PoolManager.Instance.Pop(original, parent).gameObject;
+
+        // 풀링 대상이 아니라면
+        GameObject go = Object.Instantiate(original, parent);
+        go.name = original.name;
+        return go;
+
+
+    }
+    public void Destroy(GameObject go)
+    {
+        if (go == null)
+            return;
+
+        // 풀링이 필요하다면 poolManager에 위탁
+        Poolable poolable = go.GetComponent<Poolable>();
+        if (poolable != null)
+        {
+            PoolManager.Instance.Push(poolable);
+            return;
+        }
+
+        Object.Destroy(go);
+    }
+}
